e7510498495afcb25c28a2682565110d80ed2c83
Nathan Lin <73856425+NathanLin-USF@users.noreply.github.com>
2025-11-23 23:10:47 -0500

    Add formatted output displaying calculation results

--- python_github_assignment.py
+++ python_github_assignment.py
@@ -2,4 +2,5 @@ print("Welcome to my Python program!")
 doomscroll = input("How many hours did you doomscroll today?")
 doomscroll = float(doomscroll)
 weekly_doomscroll = doomscroll * 7
-print(weekly_doomscroll)5
+print(weekly_doomscroll)
+print(f"You are on track to doomscroll {weekly_doomscroll} hours this week.")
