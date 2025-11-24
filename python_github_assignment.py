1ad86f74b615a98438a5abd5d41674e6af93e0ae
Nathan Lin <73856425+NathanLin-USF@users.noreply.github.com>
2025-11-23 23:09:38 -0500

    Add calculation based on user input

--- python_github_assignment.py
+++ python_github_assignment.py
@@ -1,2 +1,5 @@
 print("Welcome to my Python program!")
 doomscroll = input("How many hours did you doomscroll today?")
+doomscroll = float(doomscroll)
+weekly_doomscroll = doomscroll * 7
+print(weekly_doomscroll)5
